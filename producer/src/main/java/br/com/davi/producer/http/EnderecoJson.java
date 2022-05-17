18244b3b6e4d9560f9e2eef8ce7e679c108bdda6
davigomesflorencio <davigomesflorencio@gmail.com>
2022-05-16 21:40:35 -0300

    feat - Criando modelo JSON para enviar nas requisições nos end-points

---
+++ producer/src/main/java/br/com/davi/producer/http/EnderecoJson.java
@@ -0,0 +1,18 @@
+package br.com.davi.producer.http;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class EnderecoJson {
+
+    private String cep;
+    private String complemento;
+}
