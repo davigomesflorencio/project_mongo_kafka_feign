7ec79487e9cc83d3745dca93093d7637b6015fc2
davigomesflorencio <davigomesflorencio@gmail.com>
2022-05-16 21:38:50 -0300

    feat - Criando response do endereço

---
+++ producer/src/main/java/br/com/davi/producer/response/EnderecoResponse.java
@@ -0,0 +1,26 @@
+package br.com.davi.producer.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EnderecoResponse {
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
